ba476bac3020608263ad1d75e6188d2552ba3047
miaowwwww <imguodeq@163.com>
2017-02-24 15:28:25 +0800

    some update

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React, {PureComponent } from 'react';
 
-// import Model from './Model.js';
+import Model from './Model.js';
 import style from '../style/index.less';
 export default class App extends PureComponent {
 	constructor() {
 		super();
 		this.state = {name: 'miaowwwww'};
 	}
+	handleClick = () => {
+		Model.show(this.state.name)
+			.then(newname => {
+				this.setState({name: newname});
+			})
+			.catch(reject => {
+				console.log(reject);
+			}) 
+	}
+
+	/*	// 使用import()按需加载Model
 	handleClick = () => {
 		import('./Model.js')
 			.then(model => {
@@ -15,12 +26,12 @@ export default class App extends PureComponent {
 						this.setState({name: newname});
 					})
 					.catch(reject => {
-						console.log('model show error')
 						console.log(reject);
 					}) 
 			})
 			.catch(err => console.log('import error!!!!!!!!!'))
-	}
+	}*/
+
 	render() {
 		return (
 			<div className={style.bg} >
--- src/components/Model.js
+++ src/components/Model.js
@@ -9,11 +9,31 @@ export default class Model extends PureComponent {
 		// 使用this.props初始化this.state也是反模式的一种
 		this.state = {name: this.props.name};
 
+		this._reject = null;
+		this._resolve = null;
 		this._promise = new Promise((resolve, reject) => {
 			this._resolve = resolve;
 			this._reject = reject;
 		});
 		this._container = null;
+	}
+
+	static show = (name) => {
+		// 显示组件
+		let div = document.createElement('div');
+			// _model 就是当前的 Model的一个实例
+		let _model = ReactDOM.render(<Model name={name} />, div);
+		document.querySelector('body').appendChild(div);
+		
+		_model._container = div; //在_model实例中保存div的引用，销毁要用
+		// 返回一个promise
+		return _model._promise;
+	}
+
+	// 销毁 1.销毁react组件 2.销毁 div 元素
+	_removeModel = () => {
+		this._container.remove();
+		ReactDOM.unmountComponentAtNode(this._container);
 	}
 
 	handleChange = (e) => {
@@ -28,21 +48,6 @@ export default class Model extends PureComponent {
 	handleClickCancel = () => {
 		this._reject(`cancen update name`);
 		this._removeModel();
-	}
-
-	static show = (name) => {
-		let div = document.createElement('div');
-		let _model = ReactDOM.render(<Model name={name} />, div);
-		document.querySelector('body').appendChild(div);
-		// _model 就是当前的 Model的一个实例
-		_model._container = div;
-		return _model._promise;
-	}
-
-	// 销毁 1.销毁react组件 2.销毁 div 元素
-	_removeModel = () => {
-		ReactDOM.unmountComponentAtNode(this._container);
-		this._container.remove();
 	}
 
 	render() {
